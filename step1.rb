74f0389e13e32e2b3bfd29bd164908ef29ac2be9
asvnpr <vega360@gmail.com>
2016-10-23 17:08:28 -0400

    rearranged stuff in step1 and commented a few things. completed step2

--- step1.rb
+++ step1.rb
@@ -2,12 +2,15 @@ require 'uri'
 require 'net/http'
 require 'json'
 
-dict = {'token' => '57bbfceb05c5d034b1be3564c22c8c51', 'github' => 'https://github.com/asvnpr/code2040.git'}
-json_headers = {"Content-Type" => "application/json",
-                "Accept" => "application/json"}
-
+#setup URI and http
 uri = URI.parse('http://challenge.code2040.org/api/register')
 http = Net::HTTP.new(uri.host, uri.port)
 
+#initialize JSON
+dict = {'token' => '57bbfceb05c5d034b1be3564c22c8c51', 'github' => 'https://github.com/asvnpr/code2040.git'}
+json_headers = {"Content-Type" => "application/json","Accept" => "application/json"}
+
+#send JSON to URI
 response = http.post(uri.path, dict.to_json, json_headers)
+#check response from http post
 puts "response was: #{response.body}\nwith status code: #{response.code}"
---
+++ step2.rb
@@ -0,0 +1,28 @@
+###post a JSON to code2040, get a string from response, reverse the string and send it back to them
+require 'uri'
+require 'net/http'
+require 'json'
+
+#setup URI and http
+uri = URI.parse('http://challenge.code2040.org/api/reverse')
+http = Net::HTTP.new(uri.host, uri.port)
+
+#initialize JSON
+post = {'token' => '57bbfceb05c5d034b1be3564c22c8c51'}
+json_headers = {"Content-Type" => "application/json","Accept" => "application/json"}
+
+#send JSON to URI and get string to reverse
+string = http.post(uri.path, post.to_json, json_headers)
+
+#change URI and http for validation endpoint
+uri = URI.parse('http://challenge.code2040.org/api/reverse/validate')
+http = Net::HTTP.new(uri.host,uri.port)
+
+rev = string.body.to_str.reverse
+answer = {'token' => '57bbfceb05c5d034b1be3564c22c8c51',"string" => "#{rev}"}
+
+#send JSON with reversed string and capture response
+response = http.post(uri.path, answer.to_json, json_headers)
+
+#check response from http post
+puts "response was: #{response.body}\nwith status code: #{response.code}"
