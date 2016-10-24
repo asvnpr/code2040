d17119dfe4714d4b3f7f9ddf8f81cc78f514be24
asvnpr <vega360@gmail.com>
2016-10-24 02:35:30 -0400

    completed step5(final one woohoo)

--- step5.rb
+++ step5.rb
@@ -1,7 +1,8 @@
-###send post to code2040, reveive prefix string and array of string. return array with strings without the prefix
+###send post to code2040, reveive JSON with datestamp in ISO8601 format and interval (number of seconds), return datestamp + interval in appropriate format to code2040
 require 'uri'
 require 'net/http'
 require 'json'
+require 'time'
 
 #setup URI and http
 uri = URI.parse('http://challenge.code2040.org/api/dating')
@@ -11,22 +12,25 @@ http = Net::HTTP.new(uri.host, uri.port)
 post = {"token" => "57bbfceb05c5d034b1be3564c22c8c51"}
 json_headers = {"Content-Type" => "application/json","Accept" => "application/json"}
 
-#get response with prefix and array 
+#get response with datestamp and interval
 stamp_and_interval = http.post(uri.path, post.to_json, json_headers)
 
 #change URI and http for validation endpoint
-uri = URI.parse('http://challenge.code2040.org/api/prefix/validate')
+uri = URI.parse('http://challenge.code2040.org/api/dating/validate')
 http = Net::HTTP.new(uri.host,uri.port)
 
-#get prefix string and array of strings from post response
+#get prefix datestamp and interval strings from post response
 datestamp = JSON.parse(stamp_and_interval.body)["datestamp"]
 interval = JSON.parse(stamp_and_interval.body)["interval"]
 
-puts "datestamp: #{datestamp.split(':')}"
+#convert datestamp string to object, add seconds, and specify iso8601 format
+datestamp_add = (Time.parse(datestamp) + interval.to_i).iso8601
+
+
 
 #initialize correct answer dictionary and send to code 2040
-#answer = {"token" => "57bbfceb05c5d034b1be3564c22c8c51","array" => array}
-#response = http.post(uri.path, answer.to_json, json_headers)
+answer = {"token" => "57bbfceb05c5d034b1be3564c22c8c51","datestamp" => datestamp_add}
+response = http.post(uri.path, answer.to_json, json_headers)
 
 #check response from http post
-#puts "response was: #{response.body}\nwith status code: #{response.code}\nprefix is: #{prefix}\narray is: #{array}\nnon-prefix strings are: #{array}"
+puts "response was: #{response.body}\nwith status code: #{response.code}\ndatestamp is: #{datestamp}\ninterval is: #{interval}\nchanged datestamp is: #{datestamp_add}"
